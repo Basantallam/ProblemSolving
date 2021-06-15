b3011671e46a2b372be7e008d5d4631e65a69c85
Basant <30272808+Basantallam@users.noreply.github.com>
2021-06-15 17:11:12 +0200

    Update 579A- Raising Bacteria.java

--- 579A- Raising Bacteria.java
+++ 579A- Raising Bacteria.java
@@ -4,29 +4,19 @@ import java.io.InputStreamReader;
 import java.util.*;
  
 import java.util.TreeSet;
-public class Yalla {
- 
- 
-    public static void main(String[] args)  {
-    
-     
-     int n;
-  try {BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-      int c=0;
-   n = Integer.parseInt(br.readLine());
-   String b=Integer.toBinaryString(n);
+public class RaisingBacteria {
+
+    public static void main(String[] args)  {    
+       int n;
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       int c=0;
+       n = Integer.parseInt(br.readLine());
+       String b=Integer.toBinaryString(n);
   // System.out.println(b);
-   for(int i=0;i<b.length();i++) {
-    
-     if (b.charAt(i)=='1') {
-      c++;
-     }
-   }
-    System.out.println(c);
-  }
- 
-  catch (IOException e) {} 
-     
+      for(int i=0;i<b.length();i++) 
+        if (b.charAt(i)=='1') 
+          c++;
+      System.out.println(c); 
      
     }
   
