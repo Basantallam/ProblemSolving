ad4644fab13c63ee8834f535c9ea564f55b1559f
Basant <30272808+Basantallam@users.noreply.github.com>
2021-06-15 17:24:25 +0200

    Update 271A- Beautiful Year.java

--- 271A- Beautiful Year.java
+++ 271A- Beautiful Year.java
@@ -3,39 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
  
-public class Yalla {
+public class BeautifulYear {
  
  
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
     
      TreeSet <String> set=new TreeSet<>();
      boolean t=false;
      String n;
      int nn;
      String ns;
-  try {
-   BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
      n = br.readLine();
      nn=Integer.parseInt(n);
      while(true) {
-      nn++;
-             ns=String.valueOf(nn);
-   for(int i=0;i<ns.length();i++) {
-     set.add(ns.charAt(i)+"");
-     
-   }
-   //System.out.println(set);
-        if (set.size()==4) {
-        
-         System.out.println(ns);
-         break;
-        }
-        set.clear();
-  }
-  }
-  catch (IOException e) {} 
-     
-     
+       nn++;
+       ns=String.valueOf(nn);
+       for(int i=0;i<ns.length();i++) 
+         set.add(ns.charAt(i)+"");
+         if (set.size()==4) {
+           System.out.println(ns);
+           break;
+         }
+         set.clear();
+      } 
+    
     }
   
 }
