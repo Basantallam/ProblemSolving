ed62a108a0d059b93cf5af74a8d3c62dfacca59a
Basant <30272808+Basantallam@users.noreply.github.com>
2021-06-15 17:12:47 +0200

    Update 88A- Chord.cpp

--- 88A- Chord.cpp
+++ 88A- Chord.cpp
@@ -1,10 +1,4 @@
-//============================================================================
-// Name        : Chord.cpp
-// Author      : Basant
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+
  
 #include <iostream>
 #include <algorithm>
