3ddf1074e09d5d27546ffbb32ccd494fd19a126f
Basant Allam <30272808+Basantallam@users.noreply.github.com>
2021-12-06 19:40:08 +0200

    Create InvertBinaryTree.java

---
+++ LeetCode/InvertBinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    
+    public void invert(TreeNode node){
+        if(node==null)
+            return ;
+        invert(node.left);
+        invert(node.right);
+        TreeNode temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+        
+    }
+}
